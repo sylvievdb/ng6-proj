fc6ab700944f146a9d0f04604ff7bd4e60b9666a
SVDBROU1 <sylvie.vandenbroucke@vdab.be>
2018-07-28 19:53:16 +0200

    Angular 6 Animation Tutorial

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,11 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
+  animations: [
+    trigger('listStagger', [
+      transition('* <=> *', [
+        query(
+          ':enter',
+          [
+            style({opacity: 0, transform: 'translateY(-15px)'}),
+            stagger(
+              '50ms',
+              animate(
+                '550ms ease-out',
+                style({opacity: 1, transform: 'translateY(0px)'})
+              )
+            )
+          ],
+          {optional: true}
+        ),
+        query(':leave', animate('50ms', style({opacity: 0})), {
+          optional: true
+        })
+      ])
+    ])
+  ]
 })
+
 export class UsersComponent implements OnInit {
 
   users$: Object;
